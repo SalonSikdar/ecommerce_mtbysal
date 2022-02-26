d32943bd8b1743591d4e1f9fc77e8a4691fe2bbe
Saloni Sikdar <mithisikdar@gmail..com>
2022-02-26 22:11:15 +0530

    basic repository setup

---
+++ src/colorTheme.js
@@ -0,0 +1,18 @@
+const bodyDoc = document.querySelector("body");
+const rootDoc = document.querySelector(":root");
+
+bodyDoc.style.backgroundColor = "inherit";
+bodyDoc.style.color = "#3e4152";
+rootDoc.style.setProperty("--bg-color", "#FCFCFC");
+rootDoc.style.setProperty("--primary-color", "#11224D");
+rootDoc.style.setProperty("--medium-primary-color", "#193A6F");
+rootDoc.style.setProperty("--light-primary-color", "#2C599D");
+rootDoc.style.setProperty("--ultra-light-primary-color", "#5B84C4");
+rootDoc.style.setProperty("--secondary-color", "#F98125");
+rootDoc.style.setProperty("--light-secondary-color", "#FB9B50");
+rootDoc.style.setProperty("--success-color", "#3E5B31");
+rootDoc.style.setProperty("--failure-color", "#B40F36");
+rootDoc.style.setProperty("--info-color", "#0C6980");
+rootDoc.style.setProperty("--warning-color", "#EFCA2D");
+rootDoc.style.setProperty("--white-global", "#fff");
+rootDoc.style.setProperty("--black-global", "#000");
